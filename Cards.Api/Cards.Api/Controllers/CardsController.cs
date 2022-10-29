1c87de9b0e83325eb89b7bfa879b7df03c42bebb
agurokeendavid <agurokeendavid@gmail.com>
2022-10-30 01:45:35 +0800

    add update a card.

--- Cards.Api/Cards.Api/Controllers/CardsController.cs
+++ Cards.Api/Cards.Api/Controllers/CardsController.cs
@@ -27,11 +27,47 @@ namespace Cards.Api.Controllers
         // Get Single Card
         [HttpGet]
         [Route("id:guid")]
-        public async Task<IActionResult> GetAllCards([FromRoute] Guid id)
+        [ActionName("GetCard")]
+        public async Task<IActionResult> GetCard([FromRoute] Guid id)
         {
             var card = await _cardsDbContext.Cards.FirstOrDefaultAsync(x => x.Id == id);
 
             return card != null ? Ok(card) : NotFound("Card not found!");
         }
+        
+        // Add Card
+        [HttpPost]
+        public async Task<ActionResult> AddCard([FromBody] Card card)
+        {
+            card.Id = Guid.NewGuid();
+
+            await _cardsDbContext.Cards.AddAsync(card);
+            await _cardsDbContext.SaveChangesAsync();
+            
+            return CreatedAtAction(nameof(GetCard), new { id = card.Id }, card);
+        }
+        
+        // Updating a Card
+        [HttpPut]
+        [Route("id:guid")]
+        public async Task<IActionResult> UpdateCard([FromRoute] Guid id, [FromBody] Card card)
+        {
+            var existingCard = await _cardsDbContext.Cards.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if (existingCard == null) return NotFound("Card not found");
+            
+            existingCard.CardholderName = card.CardholderName;
+            existingCard.CardNumber = card.CardNumber;
+            existingCard.ExpiryMonth = card.ExpiryMonth;
+            existingCard.ExpiryYear = card.ExpiryYear;
+            existingCard.CVC = card.CVC;
+
+            await _cardsDbContext.SaveChangesAsync();
+
+            return Ok(existingCard);
+
+        }
+        
+        
     }
 }
