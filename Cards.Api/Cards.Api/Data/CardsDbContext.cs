4d9bac28ad29e17baca3a6eabf0c9fed2fad277b
agurokeendavid <agurokeendavid@gmail.com>
2022-10-30 01:36:28 +0800

    add new migration for cards table.

---
+++ Cards.Api/Cards.Api/Data/CardsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Cards.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards.Api.Data;
+
+public class CardsDbContext : DbContext
+{
+    public CardsDbContext(DbContextOptions<CardsDbContext> options) : base(options)
+    {
+    }
+    
+    // Dbset
+    public DbSet<Card> Cards { get; set; }
+    
+
+}
--- Cards.Api/Cards.Api/Program.cs
+++ Cards.Api/Cards.Api/Program.cs
@@ -1,3 +1,6 @@
+using Cards.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject Dbcontext
+builder.Services.AddDbContext<CardsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CardsDbConnectionString")));
+
 
 var app = builder.Build();
 
